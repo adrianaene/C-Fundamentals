d644731c41e7bf851f728ca94617338816cc2789
adrianaene <adriana.ene93@gmail.com>
2015-04-24 22:37:39 +0300

    temaE

---
+++ Syntax/MyHomework/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Employee : Person
+    {
+        public string dateOfEmployment;
+        public double salary;
+        public int availableDaysOff;
+        List<Leave> concedii = new List<Leave>();
+
+
+        public Employee(string lastName, string firstName, string dateOfBirth, string dateOfEmployment, double salary, int availableDaysOff)
+            : base(lastName, firstName, dateOfBirth)
+        {
+            this.salary = salary;
+            this.dateOfEmployment = dateOfEmployment;
+            this.availableDaysOff = availableDaysOff;
+        }
+
+        public void DisplayInfo()
+        {
+            //Nume, Prenume,Salariu, Numar zile disonibile
+            Console.WriteLine("Nume: " + this.lastName);
+            Console.WriteLine("Prenume: " + this.firstName);
+            Console.WriteLine("Salariu: " + this.salary);
+            Console.WriteLine("Zile libere: " + this.availableDaysOff);
+        }
+
+        private void SubstractDays(int nrZile)
+        {
+            this.availableDaysOff -= nrZile;
+        }
+
+        public void AddNewLeave(Leave leave)
+        {
+            if (this.availableDaysOff < leave.duration)
+                throw new NegativeLeaveDays("Numarul de zile ramase nu poate fi mai mare decat durata concediului");
+            SubstractDays(leave.duration);
+            leave.employee = this;
+            this.concedii.Add(leave);
+        }
+
+        public void PrintDaysOff()
+        {
+            foreach (Leave leave in this.concedii)
+                if (leave.startingDate.Contains("2015"))
+                    leave.toString();
+        }
+
+    }
+}
+
---
+++ Syntax/MyHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // in main am creat un angajat
+            // am adaugat un concediu de 5 zile si apoi unul care arunca o exceptie
+
+            Employee angajat = new Employee("Ene", "Adriana", "9.05.1993", "9.07.2015", 2000, 21);
+            Leave leave1 = new Leave("9.08.2015", 5, "Holiday");
+            Leave leave2 = new Leave("9.09.2015", 25, "Medical");
+            angajat.DisplayInfo();
+            angajat.AddNewLeave(leave1);
+            angajat.AddNewLeave(leave2);
+            Console.WriteLine("Concedii: ");
+            angajat.PrintDaysOff();
+            Console.Read();
+
+        }
+    }
+}
