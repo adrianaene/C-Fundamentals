922e76dad284c5683ca91dcebe080b1e94f760a1
adrianaene <adriana.ene93@gmail.com>
2015-04-24 22:25:51 +0300

    tema2

---
+++ Syntax/MyHomework/Leave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Leave
+    {
+        public string startingDate;
+        public int duration;
+        public string leaveType;
+        public Employee employee;
+
+        public Leave(string startingDate, int duration, string leaveType){
+            this.startingDate = startingDate;
+            this.duration = duration;
+            this.leaveType = leaveType;
+        }
+
+        public void toString()
+        {
+            Console.WriteLine("Start date: " + startingDate);
+            Console.WriteLine("Duration: " + duration);
+            Console.WriteLine("LeaveType: " + leaveType);
+        }
+    }
+}
---
+++ Syntax/MyHomework/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Person
+    {
+        protected string lastName;
+        protected string firstName;
+        protected string dateOfBirth;
+
+        public Person(string lastName, string firstName, string dateOfBirth)
+        {
+            this.lastName = lastName;
+            this.firstName = lastName;
+            this.dateOfBirth = dateOfBirth;
+        }
+    }
+}
